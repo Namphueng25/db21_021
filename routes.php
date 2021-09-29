e7ace768f46b8574a866718b6402cc16944338ad
Namphueng25 <namphueng.k@ku.th>
2021-09-29 14:35:32 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,1 +1,22 @@
+<?php
+$controllers = array('pages'=>['home','error']);
 
+if(array_key_exists($controller,$controllers))
+{
+     if(in_array($action,$controllers[$controller]))
+     {
+            call($controller.$action);
+
+     }
+     else 
+     {
+            call('pages','error');
+
+     }
+}
+else
+{
+            call('pages','error');
+
+}
+?>
