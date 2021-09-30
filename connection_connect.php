bebf5cc77da5d3b80ec06f4e0bf1993a74730cc4
Namphueng25 <namphueng.k@ku.th>
2021-09-30 14:48:08 +0700

    Update connection_connect.php

--- connection_connect.php
+++ connection_connect.php
@@ -1,1 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "db21_021";
+$password = "db21_021";
+$dbname = "db21_021";
+$conn = new mysqli($servername, $username, $password);
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+if (!$conn->select_db($dbname)) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
