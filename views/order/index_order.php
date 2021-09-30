e52bae3f06ff366db1f57783c47a94c2d66b63ac
Namphueng25 <namphueng.k@ku.th>
2021-09-30 16:20:14 +0700

    Update index_order.php

--- views/order/index_order.php
+++ views/order/index_order.php
@@ -1,1 +1,18 @@
+<table border = 1>
 
+<tr>
+<td>id</td>
+<td>date</td>
+<td>ID_Customer</td>
+<td>ID_Staff</td>
+<td>Condition</td>
+<td>Conform</td>
+</tr>
+<?php foreach($OrderList)
+
+
+
+
+
+
+</table>
