67927b809bbb08f5076609307a826be232d83753
Namphueng25 <namphueng.k@ku.th>
2021-09-30 16:59:54 +0700

    Update index_order.php

--- views/order/index_order.php
+++ views/order/index_order.php
@@ -8,11 +8,23 @@
 <td>Condition</td>
 <td>Conform</td>
 </tr>
-<?php foreach($OrderList)
+<?php foreach($order_list as $order)
+{
+        echo "<tr>
+        <td>$order->id</td>
+        <td>$order->date</td>
+        <td>$order->ID_Customer</td>
+        <td>$order->ID_Staff</td>
+        <td>$order->Condition</td>
+        <td>$order->Conform</td>
+
+        </tr>";
+}
+echo "</table>";
+?>
 
 
 
 
 
 
-</table>
