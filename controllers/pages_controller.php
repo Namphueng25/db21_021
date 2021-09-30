2a6f99bfdf70f791d82f446ec6f14f757178bc7e
Namphueng25 <namphueng.k@ku.th>
2021-09-30 14:43:01 +0700

    Update pages_controller.php

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -1,1 +1,15 @@
 
+<?php
+class PagesController
+{
+        public function home()
+        {
+                require_once('view/pages/home.php');
+        }
+        public function error()
+        {
+                require_once("views/pages/error.php");
+        }
+
+}
+?>
