c65742b36b99e3c7b3090c7ba1a85c8f9e76085f
Namphueng25 <namphueng.k@ku.th>
2021-10-01 13:10:56 +0700

    Update index_order.php

--- views/order/index_order.php
+++ views/order/index_order.php
@@ -1,11 +1,6 @@
-<table border = 1>
-
-<tr><td>id_order</td>
-<td>date</td>
-<td>condition</td>
-<td>id_cus</td>
-<td>Staff_id</td>
-<td>เงื่อนไข</td></tr>
+<table border=1>
+new order <a href=?controller=order&action=newOrder> click</a><br>
+<tr><td>id_order</td><td>date</td><td>condition</td><td>id_cus</td><td>Staff_id</td><td>เงื่อนไข</td></tr>
 <?php foreach($order_List as $order)
 {
         echo "<tr><td>$order->id_order</td><td>$order->date</td>
@@ -13,13 +8,7 @@
         <td>$order->id_cus</td>
         <td>$order->Staff_id</td>
         <td>$order->เงื่อนไข</td>
-        </tr>";
+        <td>update</td><td>delete</td></tr>";
 }
 echo "</table>";
 ?>
-
-
-
-
-
-
