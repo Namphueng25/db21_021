a4a801dd46fc5c6aa343b659315917b6ce56ae07
rossarinn <rossarin.n@ku.th>
2021-10-01 22:18:21 +0700

    Update detailOrder_controller.php

--- controllers/detailOrder_controller.php
+++ controllers/detailOrder_controller.php
@@ -1,7 +1,7 @@
 <?php class detailOrderController{
     public function index()
     {
-        $order_List=Order::getAll();
+        $detailOrder_List=detailOrder::getAll();
         require_once("./views/detailOrder/index_detailOrder.php");
     }
     
