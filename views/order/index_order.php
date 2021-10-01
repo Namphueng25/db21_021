4f2e4879ed9899adda3c4f5fd435bc6515718029
Namphueng25 <namphueng.k@ku.th>
2021-10-01 13:02:56 +0700

    Update index_order.php

--- views/order/index_order.php
+++ views/order/index_order.php
@@ -1,23 +1,18 @@
 <table border = 1>
 
-<tr>
-<td>id</td>
+<tr><td>id_order</td>
 <td>date</td>
-<td>ID_Customer</td>
-<td>ID_Staff</td>
-<td>Condition</td>
-<td>Conform</td>
-</tr>
-<?php foreach($order_list as $order)
+<td>condition</td>
+<td>id_cus</td>
+<td>Staff_id</td>
+<td>เงื่อนไข</td></tr>
+<?php foreach($order_List as $order)
 {
-        echo "<tr>
-        <td>$order->id</td>
-        <td>$order->date</td>
-        <td>$order->ID_Customer</td>
-        <td>$order->ID_Staff</td>
-        <td>$order->Condition</td>
-        <td>$order->Conform</td>
-
+        echo "<tr><td>$order->id_order</td><td>$order->date</td>
+        <td>$order->condition</td>
+        <td>$order->id_cus</td>
+        <td>$order->Staff_id</td>
+        <td>$order->เงื่อนไข</td>
         </tr>";
 }
 echo "</table>";
