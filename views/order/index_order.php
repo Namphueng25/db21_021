758b69512353af35bf54f2077d045494384c6e2b
Namphueng25 <namphueng.k@ku.th>
2021-10-01 22:44:46 +0700

    Update index_order.php

--- views/order/index_order.php
+++ views/order/index_order.php
@@ -1,5 +1,12 @@
 <table border=1>
 new order <a href=?controller=order&action=newOrder> click</a><br>
+
+<form method="get" action="">
+        <input type="text" name="key">
+        <input type="hidden" name="controller" value="order" />
+        <button type="submit" name="action" value="search"> Search </button>
+    </form>
+
 <tr><td>id_order</td><td>date</td><td>condition</td><td>id_cus</td><td>Staff_id</td><td>เงื่อนไข</td></tr>
 <?php foreach($order_List as $order)
 {
