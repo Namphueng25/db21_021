a98364bcccb16c9a87dfc8acfe4b7bcbc54a97d9
rossarinn <rossarin.n@ku.th>
2021-10-02 21:51:11 +0700

    Update detailOrder_controller.php

--- controllers/detailOrder_controller.php
+++ controllers/detailOrder_controller.php
@@ -8,11 +8,16 @@
     public function search()
     {
         $key = $_GET['key'];
-        echo $key;
         $detailOrder_List = detailOrder::search($key);
         require_once("./views/detailOrder/index_detailOrder.php");
     }
-
+    public function updateForm()
+    {
+        $id = $_GET['id'];
+        $detailOrder_List = detailOrder_List::get($id);
+       
+        require_once("./views/detailOrder/updateForm.php");
+    }
 
 
 
