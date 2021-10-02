cf34a540173ad9a8d391a3d4c61fd9d88d241e88
Namphueng25 <namphueng.k@ku.th>
2021-10-03 04:58:16 +0700

    Update deleteConform.php

--- views/order/deleteConform.php
+++ views/order/deleteConform.php
@@ -1,1 +1,23 @@
+<?php echo "<br> คุณแน่ใจนะคะว่าคุณจะลบใบเสนอราคา? (>*<) <br>
 
+            <br> $order->id_order $order->c_Fname <br>"; ?>
+
+
+
+<form method="get" action="">
+
+
+
+    <input type="hidden" name="controller" value="order" />
+
+    <input type="hidden" name="id_order" value="<?php echo $order->id_order; ?>" />
+
+    <button type="submit" name="action" value="index">back</button>
+
+    <button type="submit" name="action" value="delete">delete</button>
+
+    
+
+
+
+</form>
