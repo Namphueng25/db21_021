751e33002a27e3cdc8a464f4f7da3b50dfeaeee7
rossarinn <rossarin.n@ku.th>
2021-10-04 00:55:40 +0700

    Update deleteConFirm.php

--- views/detailOrder/deleteConFirm.php
+++ views/detailOrder/deleteConFirm.php
@@ -1,1 +1,24 @@
+<?php echo "<br> คุณแน่ใจนะคะว่าคุณจะลบใบเสนอราคา? (>*<) <br>
+
+            <br> $detailOrder->n_pid $detailOrder->no1 <br>"; ?>
+
+
+
+<form method="get" action="">
+
+
+
+    <input type="hidden" name="controller" value="detailOrder" />
+
+    <input type="hidden" name="no_id" value="<?php echo $detailOrder->no1; ?>" />
+
+    <button type="submit" name="action" value="index">back</button>
+
+    <button type="submit" name="action" value="delete">delete</button>
+
+    
+
+
+
+</form>
 
