2d689652a9e002cd2b5940f62319e284eb0cc80c
rossarinn <rossarin.n@ku.th>
2021-10-04 01:03:02 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -17,7 +17,8 @@ function call($controller,$action){
                         $controller = new OrderController();
                         break;
                         
-        case "detailOrder":      require_once("./models/order.php");
+        case "detailOrder":      echo "hi";
+                                require_once("./models/order.php");
                                 require_once("./models/detailOrder.php");
                               require_once("./models/customer.php");
                               require_once("./models/staff.php");
