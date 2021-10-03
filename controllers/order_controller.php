78b1d6ffdffbe8c87e1ae5d8d56cab3ec02213da
Namphueng25 <namphueng.k@ku.th>
2021-10-03 13:27:43 +0700

    Update order_controller.php

--- controllers/order_controller.php
+++ controllers/order_controller.php
@@ -63,21 +63,30 @@
         $id=$_GET['id_order'];
         echo "$id";
         $order = Order::get($id);
-        $starffList = starff::getAll();
+        $staffList = staff::getAll();
         $customerList = customer::getAll();
         require_once("./views/order/updateForm.php");
     }
 
     public function update()
     {
-        
+       
         $id_order=$_GET['id_order'];
+        
         $date=$_GET['date'];
         $c_id=$_GET['c_id'];
+    
         $s_id=$_GET['s_id'];
         $condition=$_GET['condition'];
         $เงื่อนไข=$_GET['เงื่อนไข'];
         $oldid=$_GET['oldid'];
+        echo $id_order;
+        echo $date;
+        echo $c_id;
+        echo $s_id;
+        echo $condition;
+        echo $เงื่อนไข;
+        echo $oldid;
         Order::Update($id_order,$date,$c_id,$s_id,$condition,$เงื่อนไข,$oldid);
 
         OrderController::index();
