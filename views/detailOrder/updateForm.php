2ca8a6dc3199e1e04d60ac15f1bbf17f62301e13
rossarinn <rossarin.n@ku.th>
2021-10-03 23:29:11 +0700

    Update updateForm.php

--- views/detailOrder/updateForm.php
+++ views/detailOrder/updateForm.php
@@ -1,43 +1,64 @@
 <ceter><form method="get" action="">
-<label>date <input type="date" name="date"
-        value="<?php echo $detailOrder->date;?>"/></label><br>
+    <label>หมายเลขใบเสนอ   <input type="number" name="no_id"
+        value="<?php echo $detailOrder->no_id;?>"/></label><br>
 
-<label>รหัสใบเสนอราคา <select name="id_order">
-    <?php foreach($orderList as $o) {
-        echo "<option value = $o->id";
-        if($o->id==$detailOrder->no1){echo " selected='selected'";}
-         echo ">$o->name</option>";}
-    ?>
-    </select></label><br> 
+    <label>วันที่ทำการสั่งซื้อ  <input type="date" name="date" 
+        value="<?php echo date("Y-m-d");?>"/></label><br>
 
-<label>รหัสสินค้า <input type="text" name="n_pid"
-        value="<?php echo $detailOrder->n_pid;?>"/></label><br>
+    <label>เปอร์เซ็นต์มัดจำ   <input type="number" name="con_payment"
+    value="<?php echo $detailOrder->con_payment;?>"/></label><br>
 
-<label>เงื่อนไขชำระ <input type="text" name="เงื่อนไข"
-        value="<?php echo $detailOrder->เงื่อนไข;?>"/></label><br>
+    <label>เงื่อนไข   <input type="text" name="เงื่อนไข"
+    value="<?php echo $detailOrder->เงื่อนไข;?>"/></label><br>
 
-<label>%มัดจำ(เครดิตเป็น0) <input type="text" name="con_payment"
-        value="<?php echo $detailOrder->con_payment;?>"/></label><br>
+    <label>รหัสstock  <input type="text" name="stock_id1"
+    value="<?php echo $detailOrder->stock_id1;?>"/></label><br>
 
-<label>รหัสพนักงาน <select name="s_id">
+    <label>รหัสพนักงาน <select name="s_id">
     <?php foreach($staffList as $s) {
-        echo "<option value = $s->id";
-        if($s->id==$detailOrder->n_staff){echo " selected='selected'";}
-         echo ">$s->name</option>";}
-    ?>
-    </select></label><br> 
-<label>รหัสลูกค้า <select name="c_id">
+        echo "<option value = $s->s_id";
+        if($s->s_id==$detailOrder->s_id){echo " selected='selected'";}
+         echo ">$s->s_id</option>";}?>
+    </select></label><br>
+
+    <label>ชื่อลูกค้า <select name="c_id">
     <?php foreach($customerList as $c) {
-        echo "<option value = $c->id";
-        if($c->id==$detailOrder->n_customer){echo " selected='selected'";}
-         echo ">$c->name</option>";}
-    ?>
+        echo "<option value = $c->c_id";
+        if($c->c_id==$detailOrder->c_id){echo " selected='selected'";}
+         echo ">$c->c_id</option>";}?>
+    </select></label><br> 
+    
+    <label>รหัสสินค้า <select name="p_id">
+    <?php foreach($productList as $p) {
+        echo "<option value = $p->p_id";
+        if($p->p_id==$detailOrder->p_id){echo " selected='selected'";}
+         echo ">$p->p_id</option>";}?>
     </select></label><br> 
 
+    <label>สี <select name="cp_color">
+    <?php foreach($colorproductList as $co) {
+        echo "<option value = $p->cp_color";
+        if($co->cp_color==$detailOrder->cp_color){echo " selected='selected'";}
+         echo ">$co->cp_color</option>";}?>
+    </select></label><br> 
 
-<input type="hidden"name="controller"value="detailOrder"/>
+    <label>จำนวน  <input type="number" name="n_pจำนวน"
+    value="<?php echo $detailOrder->n_pจำนวน;?>"/></label><br>
+    
+    <label>จำนวนสกีน  <input type="number" name="n_pscreen"
+    value="<?php echo $detailOrder->n_pscreen;?>"/></label><br>
 
-<button type= "submit"name="action"value="index">back</button>
-<button type= "submit"name="action"value="update">update</button>
+    <label>รหัสใบเสนอราคา <select name="id_order">
+    <?php foreach($ordertList as $i) {
+        echo "<option value = $i->id_order";
+        if($i->id_order==$detailOrder->id_order){echo " selected='selected'";}
+         echo ">$i->id_order</option>";}?>
+    </select></label><br> 
+
+    <input type="hidden"name="controller"value="detailOrder"/>
+<input type="hidden" name="oldid" value="<?php echo $detailOrder->no_id; ?>"/>
+<button type= "submit"name="action"value="index">Back</button>
+<button type= "submit"name="action"value="update">Update</button>
+
 
 </form>
