33b6912026c1d8dcf6ead3fb8e3a7d942f94806f
Namphueng25 <namphueng.k@ku.th>
2021-10-03 20:15:00 +0700

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,1 +1,2 @@
-<p> Welcome to our homepage </p>
+<p> Welcome to our homepage <br>
+<center><img src="https://i0.wp.com/www.beinspirational.co.uk/wp-content/uploads/2014/10/Welcome-Banner.jpg?zoom=2"  ></center></p>
