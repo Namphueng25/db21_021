2e1e84d2cf2f087277e832395c10a25e7062e4f3
Namphueng25 <namphueng.k@ku.th>
2021-10-03 12:27:08 +0700

    Update updateForm.php

--- views/order/updateForm.php
+++ views/order/updateForm.php
@@ -1,35 +1,35 @@
-<form method="get" action="">
+<form method="GET" action="">
 <label>รหัส <input type="text" name="id_order" 
         value="<?php echo $order->id_order;?>"/></label><br>
 <label>วันที่ <input type="date" name="date"
         value="<?php echo $order->date;?>"/></label><br>
 
-<label>เปอร์เซนต์ค่ามัดจำ <input type="text" name="condition" 
-        value="<?php echo $order->condition;?>"/></label><br>
-
-
- <br><label>ชื่อลูกค้า <select name="c_id">
-    <?php foreach($customerList as $C) {echo "<option value = $C->c_id>
-    $C->c_Fname</option>";}
+        <label>ชื่อลูกค้า <select name="c_id">
+    <?php foreach($customerList as $c) {
+        echo "<option value = $c->id";
+        if($c->id==$order->c_id){echo " selected='selected'";}
+         echo ">$c->c_id</option>";}
     ?>
-    </select></label><br>
+    </select></label><br> 
     
 <label>ชื่อพนักงาน <select name="s_id">
     <?php foreach($staffList as $s) {
         echo "<option value = $s->id";
         if($s->id==$order->s_id){echo " selected='selected'";}
-         echo ">$s->name</option>";}
+         echo ">$s->s_id</option>";}
     ?>
     </select></label><br>
 
 <label>เงื่อนไขชำระ <input type="text" name="เงื่อนไข"
         value="<?php echo $order->เงื่อนไข;?>"/></label><br>
 
-
+<label>เปอร์เซ็นต์มัดจำ   <input type="number" name="condition"
+        value="<?php echo $order->condition;?>"/></label><br>
+<
 
 <input type="hidden"name="controller"value="order"/>
+<input type="hidden" name="oldid" value="<?php echo $order->id_order; ?>"/>
 <button type= "submit"name="action"value="index">Back</button>
-<button type= "submit"name="action"value="update">update</button>
+<button type= "submit"name="action"value="update">Update</button>
 
 </form>
-
