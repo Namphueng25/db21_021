a6c43a9a162158863888db09b19411263743abca
Namphueng25 <namphueng.k@ku.th>
2021-10-03 16:10:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,17 +10,32 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
     $action = 'home';
 }?>
 <html>
-    <head></head>
-    <body>
+<style>
+.button {
+    background-color: #000000;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+</style>
+    <head bgcolor="Gray">
+    <h2><font size = "10">Premiumshop</font></h2>
         <?php echo "controller= ".$controller.",action=".$action;?>
-        <br>
-        <a href="http://158.108.207.4/db21/">(*--*)ปล่อยมันไป อย่างที่เป็น~~~</a>
-        &nbsp; [<a href="?controller=pages&action=home">home</a>]
-        &nbsp; [<a href="?controller=order&action=index">order</a>]
-        &nbsp; [<a href="?controller=detailOrder&action=index">detailOrder</a>]
-      <br>
-    
+        
+        <p align = "right">
+
+     <a class="button" href="http://158.108.207.4/db21/">ปล่อยมันไป อย่างที่เป็น~~~</a>
+     <a class="button" href="?controller=pages&action=home">HOME</a>
+     <a class="button" href="?controller=order&action=index">ORDER</a>
+     <a class="button" href="?controller=detailOrder&action=index">detailOrder</a>
+        </p> 
          
         <?php require_once("./routes.php");?>
-    </body>
+        </head>
 </html>
