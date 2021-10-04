b6d0c2bf376b34ed083b2d8fcdc89c10496996a0
Namphueng25 <namphueng.k@ku.th>
2021-10-04 22:38:18 +0700

    Update index_detailOrder.php

--- views/detailOrder/index_detailOrder.php
+++ views/detailOrder/index_detailOrder.php
@@ -1,3 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    border-collapse: collapse;
+    border : none;
+    width: 70%;
+}
+
+th, td {
+    text-align: center;
+    padding: 8px;
+
+}
+
+tr:nth-child(even){background-color: #F0F8FF}
+tr:hover {background-color: #CCCCFF}
+th {
+    background-color: #CC99FF;
+    color: white;
+}
+</style>
+
+<style>
+.button {
+    background-color: #CC99FF;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+</style>
+</head>
+<body bgcolor = "#F0F8FF"><center>
+
+<h2><center>ORDER TABLE</center></h2>
+
 <table border=1>
 new detailOrder <a href=?controller=detailOrder&action=newdetailOrder> click</a><br>
 
@@ -7,17 +50,19 @@ new detailOrder <a href=?controller=detailOrder&action=newdetailOrder> click</a>
         <button type="submit" name="action" value="search"> Search </button>
     </form>
 
-<tr><td>date</td>
-    <td>con_payment</td>
-    <td>เงื่อนไข</td>
-    <td>stock_id1</td>
-    <td>n_staff</td>
-    <td>n_customer</td>
-    <td>n_pid</td>
-    <td>n_pcolor</td>
-    <td>n_pจำนวน</td>
-    <td>n_pscreen</td>
-    <td>no1</td></tr>
+<tr><th>date</th>
+    <th>con_payment</th>
+    <th>เงื่อนไข</th>
+    <th>stock_id1</th>
+    <th>n_staff</th>
+    <th>n_customer</th>
+    <th>n_pid</th>
+    <th>n_pcolor</th>
+    <th>n_pจำนวน</th>
+    <th>n_pscreen</th>
+    <th>no1</th>
+    <th>UPDATE</th>
+    <th>DELETE</th></tr>
 <?php foreach($detailOrder_List as $detailOrder)
 {
         echo "<tr><td>$detailOrder->date</td>
@@ -35,6 +80,16 @@ new detailOrder <a href=?controller=detailOrder&action=newdetailOrder> click</a>
         <td><a href=?controller=detailOrder&action=deleteConFirm&no_id=$detailOrder->no_id>delete</a></td>
         </tr>"; 
 }
-echo "</table>";
 ?>
+<br>
+</center>
 
+
+
+
+
+</body>
+<br>
+<br>
+<center><font size = "4" color = "black">BY น.ส.รสริน 6220504763 </font></center>
+</html>
